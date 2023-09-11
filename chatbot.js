8e0c6a9d70f8da89831b39cb33ebbee65950f59b
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2023-09-11 19:56:12 +0530

    Chatbot updated

--- chatbot.js
+++ chatbot.js
@@ -34,7 +34,29 @@ document.addEventListener('DOMContentLoaded', function () {
         const keywordResponses = {
             'contract': 'When entering into a contract, make sure all terms are clear and agreed upon by all parties.',
             'divorce': 'Divorce proceedings can vary by location. Consult with a family law attorney for guidance.',
-            'legal advice': 'Its important to consult with a qualified attorney for legal advice tailored to your specific situation.',
+            'legal advice': 'It\'s important to consult with a qualified attorney for legal advice tailored to your specific situation.',
+            'intellectual property': 'Protect your intellectual property by registering trademarks and copyrights.',
+            'personal injury': 'If you have suffered a personal injury, consult a personal injury lawyer to explore your legal options.',
+            'employment law': 'Employment law covers various aspects of the employer-employee relationship. Consult with an employment attorney for guidance.',
+            'constitution of India': 'The Constitution of India is the supreme law of the country, which establishes the framework for the governance and legal system. It defines the fundamental rights and duties of citizens, the structure of government, and the distribution of powers between the central and state governments',
+
+            'criminal': 'Indias criminal laws are primarily defined by the Indian Penal Code (IPC). This code covers a wide range of offenses, including crimes against persons, property, and the state. Other criminal laws include the Code of Criminal Procedure (CrPC) and various special laws for specific offenses',
+            
+            'civil': 'Civil laws in India include the Indian Contract Act, which governs contracts and agreements, and the Specific Relief Act, which deals with remedies for civil wrongs. The Indian Evidence Act outlines the rules for the admissibility of evidence in legal proceedings',
+            
+            'family': 'Family law in India encompasses personal laws for different religious communities. For instance, the Hindu Marriage Act, Muslim Personal Law, and Christian Marriage Act govern aspects of marriage and family matters. The Special Marriage Act provides an alternative for inter-religious marriages',
+            
+            'property': 'Property laws in India are diverse and cover various aspects of land and property ownership. The Transfer of Property Act deals with the transfer of property rights, while the Registration Act governs the registration of documents related to land and property',
+            
+            'labor': 'India has a complex system of labor laws that regulate employment, wages, working conditions, and labor disputes. Key labor laws include the Industrial Disputes Act, Employees Provident Fund Act, and Minimum Wages Act',
+            
+            'taxation': 'The taxation system in India is governed by various laws, including the Income Tax Act, Goods and Services Tax (GST), and Customs Act. These laws specify how taxes are assessed, collected, and regulated',
+            
+            'environmental' : 'India has several environmental laws and regulations aimed at protecting the environment and natural resources. The Environmental Protection Act and Wildlife Protection Act are examples of such laws',
+            
+            'intellectual property ': 'Intellectual property rights in India are protected by laws such as the Patents Act, Copyright Act, Trademarks Act, and Designs Act. These laws govern patents, copyrights, trademarks, and industrial designs',
+            
+            'cyber': 'With the growth of the digital age, India has introduced cyber laws to address cybercrimes and online activities. The Information Technology Act deals with electronic transactions, data protection, and cybersecurity.',
             // Add more keywords and responses as needed
         };
 
